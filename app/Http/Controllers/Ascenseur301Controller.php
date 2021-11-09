22f405edfd43205c4dd31795b1385ebf7fdac2a9
Flex <71741422+flextoutcourt@users.noreply.github.com>
2021-11-09 12:28:40 +0100

    update card generator

--- app/Http/Controllers/Ascenseur301Controller.php
+++ app/Http/Controllers/Ascenseur301Controller.php
@@ -8,34 +8,36 @@ use Illuminate\Support\Facades\DB;
 
 class Ascenseur301Controller extends Controller
 {
-    public function verify_card()
+    /**
+     * verify if a card's array can exist
+     *
+     * @return void
+     */
+    public function verify_card(array $cards = []):void
     {
-        $numerosATester = [
-            "348678756",
-            "348678757",
-            "834094922",
-            "451001077",
-            "451001078",
-            "531976157",
-            "34867875600068",
-            "53197615700010",
-            "53197615700011",
-            "80508223700019",
-            "83409492200012",
-            "83409592200012",
-            "89092299000019",
-            "89092290900019",
-            "4513325199993100",
-        ];
-
-        foreach($numerosATester as $key => $item){
+        foreach($cards as $key => $item){
             echo $item.' est '. ($this->verify($item) ? 'valide' : 'invalide');
             echo '<br/>';
         }
-        // echo $this->generate(9999);
     }
 
-    private function verify($number)
+    /**
+     * generate cards that complies with th ecard validation rules
+     *
+     * @return void
+     */
+    public function card_generator():void
+    {
+        echo $this->generate(9999);
+    }
+
+    /**
+     * Return if the card number is valid
+     *
+     * @param int $number
+     * @return bool
+     */
+    private function verify($number):bool
     {
         $number = strrev($number);
         $limit = strlen($number);
@@ -54,15 +56,24 @@ class Ascenseur301Controller extends Controller
         return ($total % 10 == 0) ? true: false;
     }
 
-    public function generate($max){
+    /**
+     * Generate random cards within the last created card and the limit fixed
+     *
+     * @param integer $max
+     * @return void
+     */
+    public function generate(int $max = 9999):void
+    {
         //generer un numéro de carte bleu valide
-        $start = 5355 ;
-        for($i = 0; $i <= $max; $i++){
+        $lastCardEntered = Cards::order_by('number', 'desc')->first();
+        $start = 5355;
+        for($i = $lastCardEntered; $i <= ($lastCardEntered + $max); $i++){
             $numero = $start.str_pad($i, 12, '0', STR_PAD_LEFT);
             if($this->verify($numero)){
                 $card = Cards::firstOrCreate(['number' => $numero]);
                 $card->number = $numero;
                 $card->save();
+                echo $card.'<br />';
             }
         }
     }
